3755db27549eeaced687f70a3606e886811a14b1
Tanaka Kensaku <tannakaken@gmail.com>
2022-09-08 11:22:10 +0900

    とりあえず動くもの

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import * as THREE from "three";
+import { VRButton, XR, Controllers, Hands } from "@react-three/xr";
+import { useLoader, Canvas } from "@react-three/fiber";
 
 function App() {
+  const texture = useLoader(THREE.TextureLoader, "front.jpg");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: "100vh" }}>
+      <VRButton />
+      <Canvas>
+        <XR>
+          <Controllers />
+          <Hands />
+          <mesh>
+            <boxGeometry />
+            <meshBasicMaterial color="blue" />
+          </mesh>
+          <mesh>
+            <sphereBufferGeometry attach="geometry" args={[500, 60, 40]} />
+            <meshBasicMaterial
+              attach="material"
+              map={texture}
+              side={THREE.BackSide}
+            />
+          </mesh>
+        </XR>
+      </Canvas>
     </div>
   );
 }
