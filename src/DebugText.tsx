0f40aa72f01b4701d6f32f0e0c318ec0109256ee
Tanaka Kensaku <tannakaken@gmail.com>
2022-10-09 22:10:33 +0900

    テスト

--- src/DebugText.tsx
+++ src/DebugText.tsx
@@ -1,16 +1,12 @@
 import React, { useState } from "react";
 import { Text } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
-import { useController } from "@react-three/xr";
 
 const DebugText = () => {
-  const gazeController = useController("none");
   const [value, setValue] = useState(0);
+
   useFrame((state) => {
-    //setValue(state.camera.rotation.y);
-    if (gazeController) {
-      setValue(gazeController.rotation.y);
-    }
+    setValue(state.camera.rotation.y);
   });
   return (
     <Text
